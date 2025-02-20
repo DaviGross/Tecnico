b882d3167dc9f5a21f00c7c618a6853ead1c3262
DaviGross <davi.marafiao@escola.pr.gov.br>
2025-02-20 08:36:37 -0300

    Add files via upload

---
+++ davi2.0/array.js
@@ -0,0 +1,4 @@
+const numero = [10, 5, 78, 89, 98]
+const maior = Math.max(...numero)
+
+console.log(maior)
---
+++ davi2.0/invert.js
@@ -0,0 +1,5 @@
+let str = "javascript"
+
+ let invertida = str.split("").reverse().join("");
+
+ console.log(invertida)
