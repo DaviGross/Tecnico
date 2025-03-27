87b6ff0ec7adc507b34b81bb7e63be9fd2056d59
DaviGross <davi.marafiao@escola.pr.gov.br>
2025-03-27 07:27:35 -0300

    Add files via upload

---
+++ PHP/arrays.php
@@ -0,0 +1,21 @@
+<?php
+
+$linguagens = ["PHP", "C#", "Java"];
+
+$linguagens2 = array("PHP", "C#", "Java");
+
+$curso = [
+
+"nome_curso" =>"Curso de Fundamentos PHP",
+"versao_ferramntas" => 8.1,
+"carga_horario" => 40,
+"status" => true
+
+];
+$numeros = [
+    [1,2,3,4,],
+    [5,6,7,8,9,],
+    [10,[11,12,13]]
+];
+
+echo $numeros[1][2];
---
+++ PHP/tipos.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "Davi";
+$idade = 17;
+$casado = false;
+$preco = 100000.99;
+
+echo "A variavel é do tipo: ";
+echo gettype($nome);
+$nome = false;
+echo "<br>A Variavel é do tipo: ";
+echo gettype($nome);
